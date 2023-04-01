88580f09707b21023f544c3712db361cf19443df
Mykyta Vtorenko <limeniye@gmail.com>
2023-04-01 17:11:46 +0300

    client side

--- GreeterClient/Program.cs
+++ GreeterClient/Program.cs
@@ -1,20 +1,24 @@
 ﻿using Contracts;
-using Grpc.Net.Client;
+using Grpc.Core;
 // .. TODO :
 // .. Console.ReadKey() ->
 // .. Waiting for server initialization
 Console.ReadKey();
 const string host = "localhost";
-const string port0 = "5001"; // greeterservice
-const string port1 = "50050"; // nginx
+const string greeterservicePort = "5001"; // greeterservice
+const string nginxPort = "50050"; // nginx
+const int envoyPort = 8811; // envoy
 
 try
 {
-    var handler = new HttpClientHandler();
-    handler.ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true;
-    var options = new GrpcChannelOptions() { HttpHandler = handler };
+    //var handler = new HttpClientHandler();
+    //handler.ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true;
+    //var options = new GrpcChannelOptions() { HttpHandler = handler };
 
-    var client = new Greeter.GreeterClient(GrpcChannel.ForAddress($"http://{host}:{port1}", options));
+    var client = new Greeter.GreeterClient(new Channel(host, envoyPort, ChannelCredentials.Insecure, new List<ChannelOption> 
+    {
+        new ChannelOption("grpc.enable_http_proxy", 0) 
+    }));
     var response = client.SayHello(new () { Name = "limeniye" });
     Console.WriteLine(response.Message);
 }
