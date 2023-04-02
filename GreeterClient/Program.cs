b2d4fdeb5a559349d4d4d00e62d06cac252965e7
Mykyta Vtorenko <limeniye@gmail.com>
2023-04-02 19:22:01 +0300

    Envoy working on AWS server

--- GreeterClient/Program.cs
+++ GreeterClient/Program.cs
@@ -1,10 +1,11 @@
 ﻿using Contracts;
+using Grpc.Core;
 using Grpc.Net.Client;
 // .. TODO :
 // .. Console.ReadKey() ->
 // .. Waiting for server initialization
 Console.ReadKey();
-var channel = GrpcChannel.ForAddress("http://localhost:8080");
+var channel = GrpcChannel.ForAddress("http://3.122.94.127:8080");
 var client = new Greeter.GreeterClient(channel);
 var response = client.SayHello(new() { Name = "limeniye" });
 Console.WriteLine(response.Message);
