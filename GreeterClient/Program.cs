d12a0da153e145c3a7bc64625a4b9b6e8157a018
Mykyta Vtorenko <limeniye@gmail.com>
2023-04-05 02:27:30 +0300

    localhost

--- GreeterClient/Program.cs
+++ GreeterClient/Program.cs
@@ -5,7 +5,7 @@ using Grpc.Net.Client;
 // .. Console.ReadKey() ->
 // .. Waiting for server initialization
 Console.ReadKey();
-var channel = GrpcChannel.ForAddress("http://3.122.94.127:8080");
+var channel = GrpcChannel.ForAddress("http://localhost:8080");
 var client = new Greeter.GreeterClient(channel);
 var response = client.SayHello(new() { Name = "limeniye" });
 Console.WriteLine(response.Message);
